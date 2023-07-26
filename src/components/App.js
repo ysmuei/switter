e7c590b8e2d20e09544f705d319ddb9db6931cf5
ysmuei <108169494+ysmuei@users.noreply.github.com>
2023-07-27 02:49:19 +0900

    두번째

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <AppRouter isLoggedIn={isLoggedIn}/>
+  );
+}
+
+export default App;
